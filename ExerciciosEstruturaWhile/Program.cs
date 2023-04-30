838573d1dc6d2b33e4c23a6af8f7a98f02f40ec4
Jean Marques <jmichelmarques21@outlook.com>
2023-04-30 02:00:39 -0300

    ...

--- ExerciciosEstruturaWhile/Program.cs
+++ ExerciciosEstruturaWhile/Program.cs
@@ -5,7 +5,23 @@ namespace ExerciciosEstruturaWhile {
     class Program {
         static void Main(string[] args) {
 
+            /* Exercício 01 - Escreva um programa que repita a leitura de uma senha até que ela seja válida. Para cada leitura de senha
+             * incorreta informada, escrever a mensagem "Senha Inválida!". Quando a senha for informada corretamente deve ser
+             * impressa a mensagem "Acesso permitido!" e o algoritmo encerrado. Considere que a senha correta é o valor 2002.
 
+
+            string senha;
+
+            Console.Write("Digite a senha: ");
+            senha = Console.ReadLine();
+
+            while (senha != "2002") {
+                Console.WriteLine("Senha Inválida!");
+                Console.Write("Digite a senha: ");
+                senha = Console.ReadLine();
+            }
+
+            Console.WriteLine("Acesso permitido!"); */
 
 
         }
