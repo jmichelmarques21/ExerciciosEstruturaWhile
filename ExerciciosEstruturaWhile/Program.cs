29dfea66c0b69fc16b07c098c8187db744619675
Jean Marques <jmichelmarques21@outlook.com>
2023-04-30 02:11:13 -0300

    a

--- ExerciciosEstruturaWhile/Program.cs
+++ ExerciciosEstruturaWhile/Program.cs
@@ -24,6 +24,36 @@ namespace ExerciciosEstruturaWhile {
             Console.WriteLine("Acesso permitido!"); */
 
 
+
+            /* Exercício 02 - Escreva um programa para ler as coordenadas (X,Y) de uma quantidade indeterminada de pontos no sistema
+             * cartesiano. Para cada ponto escrever o quadrante a que ele pertence. O algoritmo será encerrado quando pelo
+             * menos uma de duas coordenadas for NULA (nesta situação sem escrever mensagem alguma). 
+
+            int x, y;
+
+            Console.WriteLine("Escreva dois valores inteiros (na mesma linha): ");
+            string[] var = Console.ReadLine().Split(' ');
+            x = int.Parse(var[0]);
+            y = int.Parse(var[1]);
+
+            while (x != 0 && y!= 0) {
+                if (x > 0 && y > 0) {
+                    Console.WriteLine("Primeiro quadrante!");
+                }
+                else if (x < 0 && y > 0) {
+                    Console.WriteLine("Segundo quadrante!");
+                }
+                else if (x < 0 && y < 0) {
+                    Console.WriteLine("Terceiro quadrante!");
+                }
+                else {
+                    Console.WriteLine("Quarto quadrante!");
+                }
+                string[] vari = Console.ReadLine().Split(' ');
+                x = int.Parse(vari[0]);
+                y = int.Parse(vari[1]);
+            } */
+
         }
     }
 }
